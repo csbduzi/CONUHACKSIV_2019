e34e4cd707537a005345211cc6f68ab955153332
csbduzi <cbahenduzi@gmail.com>
2019-01-27 00:19:06 -0500

    Add the enter button, the reset button and the progress bar.

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progess_bar: UIView!
+    var viewWidthDividedBy6 : Int = 0
+    var viewWidth : Int = 0
+    var progressBarWidth : Int = 0
+    var chances : Int = 0
+    var displayed_chances : Int = 6
+    var numberOfChances : Int = 6
+    var name_company : String?
+    
+    @IBOutlet weak var enterBtn: UIButton!
+    @IBOutlet weak var restartBtn: UIButton!
+    @IBOutlet weak var point_chances: UILabel!
+    @IBOutlet weak var inputField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        point_chances.text = "\(displayed_chances)/\(numberOfChances)"
+        
+        // restart button
+        restartBtn.isHidden = true
+        restartBtn.layer.cornerRadius = 5
+        
+        // start button
+        enterBtn.layer.cornerRadius = 5
+        
+      
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func updateUI(){
+     
+        let divided_viewWidth = self.view.frame.width / CGFloat(numberOfChances)
+        progess_bar.frame.size.width = (divided_viewWidth * CGFloat(chances))
+        
+    }
+    
+    @IBAction func pressBtn(_ sender: UIButton) {
+        
+        if sender.tag == 1{
+            
+        if(chances<numberOfChances){
+        chances=chances+1
+        displayed_chances = displayed_chances - 1
+        }
+        else{
+            restartBtn.isHidden = false
+        }
+        point_chances.text = "\(displayed_chances)/\(numberOfChances)"
+        
+        updateUI()
+       
+        }else if sender.tag == 2 {
+            // reset the number of the chances 
+            chances = 0
+            
+            point_chances.text = "\(chances)/\(numberOfChances)"
+            restartBtn.isHidden = true
+        }
+    }
+    
 }
 
